67c983bfdf8344b8f5da4b1cd0deaa4f639df7be
Joel Joseph <joeljosephjin@gmail.com>
2021-02-06 19:46:28 +0530

    more exps

--- launch_experiment.py
+++ launch_experiment.py
@@ -9,18 +9,26 @@ import click
 import json
 import torch
 
+# a lot of different stuff from different environments, big-ness necessary
 from rlkit.envs import ENVS
+# a lot of wrappers within wrappers, big-ness necessary
 from rlkit.envs.wrappers import NormalizedBoxEnv
+# also big
 from rlkit.torch.sac.policies import TanhGaussianPolicy
+# also big
 from rlkit.torch.networks import FlattenMlp, MlpEncoder, RecurrentEncoder
-# from rlkit.torch.sac.sacwb import PEARLSoftActorCritic
+# the main file, but its too big too
 from rlkit.torch.sac.sac import PEARLSoftActorCritic
+# mostly a network thing
 from rlkit.torch.sac.agent import PEARLAgent
+# it logs a lot o stuff; dont know if its even needed
 from rlkit.launchers.launcher_util import setup_logger
+# some useful tiny util functions
 import rlkit.torch.pytorch_util as ptu
+# dict of config stuff
 from configs.default import default_config
 
-
+# seed-ings
 import random
 random.seed(0)
 torch.manual_seed(0)
@@ -142,7 +150,96 @@ def main(config, gpu, docker, debug):
         variant = deep_update_dict(exp_params, variant)
     variant['util_params']['gpu_id'] = gpu
 
-    experiment(variant)
+    # experiment(variant)
+
+    # create multi-task environment and sample tasks
+    env = NormalizedBoxEnv(ENVS[variant['env_name']](**variant['env_params']))
+    tasks = env.get_all_task_idx()
+    obs_dim = int(np.prod(env.observation_space.shape))
+    action_dim = int(np.prod(env.action_space.shape))
+    reward_dim = 1
+
+    # instantiate networks
+    latent_dim = variant['latent_size']
+    context_encoder_input_dim = 2 * obs_dim + action_dim + reward_dim if variant['algo_params']['use_next_obs_in_context'] else obs_dim + action_dim + reward_dim
+    context_encoder_output_dim = latent_dim * 2 if variant['algo_params']['use_information_bottleneck'] else latent_dim
+    net_size = variant['net_size']
+    recurrent = variant['algo_params']['recurrent']
+    encoder_model = RecurrentEncoder if recurrent else MlpEncoder
+
+    context_encoder = encoder_model(
+        hidden_sizes=[200, 200, 200],
+        input_size=context_encoder_input_dim,
+        output_size=context_encoder_output_dim,
+    )
+    qf1 = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + action_dim + latent_dim,
+        output_size=1,
+    )
+    qf2 = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + action_dim + latent_dim,
+        output_size=1,
+    )
+    vf = FlattenMlp(
+        hidden_sizes=[net_size, net_size, net_size],
+        input_size=obs_dim + latent_dim,
+        output_size=1,
+    )
+    policy = TanhGaussianPolicy(
+        hidden_sizes=[net_size, net_size, net_size],
+        obs_dim=obs_dim + latent_dim,
+        latent_dim=latent_dim,
+        action_dim=action_dim,
+    )
+    agent = PEARLAgent(
+        latent_dim,
+        context_encoder,
+        policy,
+        **variant['algo_params']
+    )
+    algorithm = PEARLSoftActorCritic(
+        env=env,
+        train_tasks=list(tasks[:variant['n_train_tasks']]),
+        eval_tasks=list(tasks[-variant['n_eval_tasks']:]),
+        nets=[agent, qf1, qf2, vf],
+        latent_dim=latent_dim,
+        **variant['algo_params']
+    )
+
+    # optionally load pre-trained weights
+    if variant['path_to_weights'] is not None:
+        path = variant['path_to_weights']
+        context_encoder.load_state_dict(torch.load(os.path.join(path, 'context_encoder.pth')))
+        qf1.load_state_dict(torch.load(os.path.join(path, 'qf1.pth')))
+        qf2.load_state_dict(torch.load(os.path.join(path, 'qf2.pth')))
+        vf.load_state_dict(torch.load(os.path.join(path, 'vf.pth')))
+        # TODO hacky, revisit after model refactor
+        algorithm.networks[-2].load_state_dict(torch.load(os.path.join(path, 'target_vf.pth')))
+        policy.load_state_dict(torch.load(os.path.join(path, 'policy.pth')))
+
+    # optional GPU mode
+    ptu.set_gpu_mode(variant['util_params']['use_gpu'], variant['util_params']['gpu_id'])
+    if ptu.gpu_enabled():
+        algorithm.to()
+
+    # debugging triggers a lot of printing and logs to a debug directory
+    DEBUG = variant['util_params']['debug']
+    os.environ['DEBUG'] = str(int(DEBUG))
+
+    # create logging directory
+    # TODO support Docker
+    exp_id = 'debug' if DEBUG else None
+    experiment_log_dir = setup_logger(variant['env_name'], variant=variant, exp_id=exp_id, base_log_dir=variant['util_params']['base_log_dir'])
+
+    # optionally save eval trajectories as pkl files
+    if variant['algo_params']['dump_eval_paths']:
+        pickle_dir = experiment_log_dir + '/eval_trajectories'
+        pathlib.Path(pickle_dir).mkdir(parents=True, exist_ok=True)
+
+    # run the algorithm
+    algorithm.train()
 
 if __name__ == "__main__":
     main()
